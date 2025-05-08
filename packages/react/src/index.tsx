8bad604b48c5e852c8fcf14cc6621f6f5327a703
Glenn Töws <glenntoews@outlook.com>
2025-05-08 09:25:55 +0200

    fix(react): prevent multiple initializations of StagewiseToolbar

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,13 @@
 import { initToolbar, type ToolbarConfig } from '@stagewise/toolbar';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export type { ToolbarConfig } from '@stagewise/toolbar';
 
 export function StagewiseToolbar({ config }: { config: ToolbarConfig }) {
+  const initFlag = useRef(false);
   useEffect(() => {
+    if (initFlag.current) return;
+    initFlag.current = true;
     initToolbar(config);
   }, [config]);
 
