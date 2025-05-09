a81446ab6798e522092026e4a5ea20d371faa64a
Glenn Töws <glenntoews@outlook.com>
2025-05-09 22:15:10 +0200

    chore(playgrounds): remove next-playground example and related files

--- examples/next-example/src/app/layout.tsx
+++ examples/next-example/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StagewiseToolbar />
+        <StagewiseToolbar config={{ plugins: [] }} />
         {children}
       </body>
     </html>
--- playgrounds/next-playground/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# next-playground
-
-## 0.1.1
-
-### Patch Changes
-
-- Updated dependencies
-  - @stagewise/toolbar@0.1.0
