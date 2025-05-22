fe3af108ef81b553fea3d690d51cc8fae8ff4371
Glenn Töws <glenntoews@outlook.com>
2025-05-22 22:29:25 +0200

    feat(react-plugin): enhance context element interactions and improve component hierarchy information
    - Introduced `onContextElementHover` and updated `onContextElementSelect` to provide detailed annotations for hovered and selected React components.
    - Enhanced the `getSelectedElementsPrompt` function to format and return additional information about selected elements' React component hierarchies.
    - Updated the UI components to display context information more effectively, improving user experience when interacting with the toolbar.

--- plugins/react/src/index.tsx
+++ plugins/react/src/index.tsx
@@ -1,20 +1,50 @@
 'use client';
 import type { ToolbarPlugin } from '@stagewise/toolbar';
 import { ReactLogo } from './logo';
-import { getReactComponentName } from './utils';
-import { ExampleComponent } from './component';
+import { getReactComponentHierarchy } from './utils';
+
+const getSelectedElementAnnotation = (element: HTMLElement) => {
+  const annotation = getReactComponentHierarchy(element)[0]?.name;
+  return { annotation: annotation ?? null };
+};
 
 export const ReactPlugin: ToolbarPlugin = {
   displayName: 'React',
   description:
-    'This toolbar adds basic analytical and UI support for apps using React as a UI framework',
+    'This toolbar adds additional information and metadata for apps using React as a UI framework',
   iconSvg: <ReactLogo />,
   pluginName: 'react',
-  onContextElementSelect: (element) => {
-    const annotation = getReactComponentName(element);
-    return {
-      annotation,
-    };
-  },
-  onActionClick: () => <ExampleComponent />,
+  onContextElementHover: getSelectedElementAnnotation,
+  onContextElementSelect: getSelectedElementAnnotation,
+  onPromptSend: (prompt) => ({
+    contextSnippets: [
+      {
+        promptContextName: 'elements-react-component-info',
+        content: getSelectedElementsPrompt(prompt.contextElements),
+      },
+    ],
+  }),
+};
+
+const getSelectedElementsPrompt = (elements: HTMLElement[]) => {
+  const selectedComponentHierarchies = elements.map((e) =>
+    getReactComponentHierarchy(e),
+  );
+
+  if (selectedComponentHierarchies.some((h) => h.length > 0)) {
+    const content = `This is additional information on the elements that the user selected. Use this information to find the correct element in the codebase.
+
+  ${selectedComponentHierarchies.map((h, index) => {
+    return `
+<element index="${index + 1}">
+  ${h.length === 0 ? 'No React component as parent detected' : `React component tree (from closest to farthest, 3 closest elements): ${h.map((c) => `{name: ${c.name}, type: ${c.type}}`).join(' child of ')}`}
+</element>
+    `;
+  })}
+  `;
+
+    return content;
+  }
+
+  return null;
 };
