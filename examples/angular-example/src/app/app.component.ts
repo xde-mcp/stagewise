35f7e54402bf8057319d6c947f5341db19330996
Glenn Töws <glenntoews@outlook.com>
2025-06-05 20:27:45 +0200

    chore(angular-example): build multi-component app structure

--- examples/angular-example/src/app/app.component.ts
+++ examples/angular-example/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ResourcesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
