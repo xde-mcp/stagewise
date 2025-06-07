9f5bd4d3f5900eaa7205e5f228d5e7fbc4ba5882
Glenn Töws <glenntoews@outlook.com>
2025-06-07 21:24:03 +0200

    refactor(toolbar): remove unused hooks and dependencies
    - Deleted several unused hooks: useElementSize, useExtension, useKeyBinding, useLocation, usePreferMobileLayout, and useTimeAgo.
    - Removed javascript-time-ago dependency from package.json.

--- toolbar/core/src/hooks/use-prefer-mobile-layout.tsx
+++
@@ -1,6 +0,0 @@
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-import { useMediaQuery } from '@/hooks/use-media-query';
-
-export const usePreferMobileLayout = () => useMediaQuery('(max-width: 768px)');
