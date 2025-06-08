bed0d3bfa54a46f543be439c535e8fdbcda49ed4
Glenn Töws <glenntoews@outlook.com>
2025-06-08 21:36:32 +0200

    feat(website): add links for social networks

--- apps/website/src/app/layout.config.tsx
+++ apps/website/src/app/layout.config.tsx
@@ -2,6 +2,7 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import Image from 'next/image';
 import StagewiseLogo from './logo.svg';
 import StagewiseLogoWhite from './logo-white.svg';
+import { SiDiscord, SiGithub, SiX } from 'react-icons/si';
 /**
  * Shared layout configurations
  *
@@ -33,6 +34,27 @@ export const baseOptions: BaseLayoutProps = {
       text: 'Documentation',
       url: '/docs',
       active: 'nested-url',
+    },
+    {
+      type: 'icon',
+      label: 'Discord', // `aria-label`
+      icon: <SiDiscord className="m-1 size-4" />,
+      text: 'Discord',
+      url: 'https://discord.gg/9dy3YSE8',
+    },
+    {
+      type: 'icon',
+      label: 'X', // `aria-label`
+      icon: <SiX className="m-1 size-4" />,
+      text: 'X',
+      url: 'https://x.com/stagewise_io',
+    },
+    {
+      type: 'icon',
+      label: 'GitHub', // `aria-label`
+      icon: <SiGithub className="m-1 size-4" />,
+      text: 'itHub',
+      url: 'https://github.com/stagewise-io/stagewise',
     },
   ],
 };
